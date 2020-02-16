d6e49f64fb03c5f8c85cb9b092574f62ccd32fa2
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-02-15 22:43:39 -0500

    fix 1 for multiple method calling

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const Clancy = require('./src/Clancy')
 
+
+
 const test = new Clancy('test')
 
-console.log(test.getdbname())
+// for(let x =0;x<100;x++){
+//     console.log(test.getdbname())
+// }
 
-test.insert({fullName: `Nabeel`}, ()=>{})
-test.insert({fullName: `Shard`}, ()=>{})
+test.insert({fullName: `Nabeel`})
